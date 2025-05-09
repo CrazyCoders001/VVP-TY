13f198dae9a3e880573567ade2d9226103967eef
CrazyCoders001 <akasgkanni45@gmail.com>
2025-05-09 19:33:52 +0530

    Create HelloCoders.cpp

---
+++ CSE/Notes/Fundamentals/HelloCoders.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>  
+//this is used to import built in functions
+using namespace std;
+
+int main(){ //function/code block start with open curly brace
+
+    cout<<"Hello coders from TY";
+    // semicolen denotes end of an statement
+    return 0;
+} //function/code block end with close curly brace
